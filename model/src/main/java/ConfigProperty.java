37355570ee0cf70e1313bb4637765772637a254a
AlenaUslontseva <auslonceva@ya.ru>
2021-01-26 17:03:32 +0300

    review fixes

--- model/src/main/java/ConfigProperty.java
+++ model/src/main/java/ConfigProperty.java
@@ -1,6 +1,7 @@
 import org.aeonbits.owner.Config;
 
+@Config.Sources("classpath:ConfigProperty.properties")
 public interface ConfigProperty extends Config {
+    @Key("otusUrl")
     String otusUrl();
-
 }
